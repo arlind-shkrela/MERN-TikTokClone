478bd735bf145dbde2c38c4a9b81482b485f3667
Arlind Shkrela <arlind.shkrela@outlook.com>
2021-06-09 09:43:42 +0200

    tiktokSchema and endpoints

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <div className="app_videos">
-          <Video url ={"https://v16m.tiktokcdn.com/01f4ec4102dd29da9020002906c5eab3/60bce8e9/video/tos/alisg/tos-alisg-pve-0037/f57a3fb7de134bbeb6d0734ff0b4fbb2/?a=1180&br=2132&bt=1066&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=3&ds=3&er=&l=202106060925090101890710305106DF58&lr=tiktok&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3BveDk3d2l1NTMzODgzM0ApOjs8PGRpNWVkNzRlPDQ5OWcwXmRlZjA0NmdgLS1kLzRzc182MV4yNDJiYjYuLWAvMmI6Yw%3D%3D&vl=&vr="} 
+          <Video url ={"https://v16m.tiktokcdn.com/1786308703739b8527ec172cf355c121/60bf9419/video/tos/useast2a/tos-useast2a-ve-0068c004/d031e75a64884de08cfd88254a857ceb/?a=1233&br=1544&bt=772&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106081000150101890731325004F5C3&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=am9obzZuOTxoNTMzNzczM0ApNGY4aDhpNmU5Nzc6OWQ4ZGdya2c2M3FnaWJgLS1kMTZzczItX2FgMDUtY141NGEyMS46Yw%3D%3D&vl=&vr="} 
           channel ={"arlindi"}
           messages = {120}
           likes = {210}
@@ -14,7 +14,7 @@ function App() {
           song = {"this is song name "}
 
           />
-          <Video url ={"https://v16m.tiktokcdn.com/a33f19379b9ff323519604681279e249/60bd0895/video/tos/useast2a/tos-useast2a-pve-0068/dbbc70a725a34a8583bb904e5f2ea37b/?a=1233&br=1748&bt=874&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106061140040101890651350802A8FC&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajZvZDxmOmVtNDMzMzczM0ApNGQ6ZzQ4OTszN2c5NmZmZmdrNmRtNmVrc2JgLS02MTZzc2AzX2M0YDNhX2A1LjEwMV46Yw%3D%3D&vl=&vr="} 
+          <Video url ={"https://v16m.tiktokcdn.com/6b30c8ad8778ffc5a46dcd9001c8f427/60bf937f/video/tos/useast2a/tos-useast2a-ve-0068c003/8946f6b0ccd84a6cb33f5a868c8b57bb/?a=1233&br=4028&bt=2014&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106080957420101901871630B04EF0A&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajx5eXc8Omd4NTMzNzczM0ApZ2U7PDVnNmVoN2c1ZWdlZGcuaGtrcTMyLXJgLS1kMTZzc2I0NC1hLWEyLy9jLS5hNGA6Yw%3D%3D&vl=&vr="} 
           channel ={"leandra"}
           messages = {430}
           likes = {510}
